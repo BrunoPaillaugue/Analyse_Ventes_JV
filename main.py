f02d8d3f01743ea3f9a49c14a5b4c9139a113946
Paillaugue bruno <paillaugue.bruno@gmail.com>
2026-09-04 21:21:38 -0400

    data cleaning for Publisher

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ pd.set_option('display.max_rows', 100)
 df = pd.read_csv('data/vgsales.csv')
 print(df.info())
 print(df.isnull().sum())
+
+# Year : valeurs manquantes volontairement conservées  pour être automatiquement exclues des analyses plutôt que remplacées par une valeur arbitraire
+
+def fill_na_publisher(df):
+    return df["Publisher"].fillna("Unknown")
+
+df = fill_na_publisher(df)
+print(df.isnull().sum())
