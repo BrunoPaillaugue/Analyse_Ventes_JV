d1b635549eec178b2e2b123f8a9255bcc733eac6
Paillaugue bruno <paillaugue.bruno@gmail.com>
2026-09-04 22:05:54 -0400

    Add aggregation functions

--- main.py
+++ main.py
@@ -3,14 +3,44 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_rows', 100)
 
-df = pd.read_csv('data/vgsales.csv')
-print(df.info())
-print(df.isnull().sum())
 
-# Year : valeurs manquantes volontairement conservées  pour être automatiquement exclues des analyses plutôt que remplacées par une valeur arbitraire
+
+# Year : missing values keeped to ignore them
 
 def fill_na_publisher(df):
     return df["Publisher"].fillna("Unknown")
 
-df = fill_na_publisher(df)
+# Return the top 10 sales in global
+def top_sales_global(df):
+    df.sort_values("Global_Sales",ascending=False)
+    return df.head(10)
+
+# Return the global Sales of each Genre, sorted
+def sales_by_genres(df):
+    return df.groupby("Genre")["Global_Sales"].sum().sort_values(ascending=False)
+
+# Return the evolution of Global_Sales in the years
+def sales_by_year(df):
+    return df.groupby("Year")["Global_Sales"].sum().sort_index()
+
+def sales_by_region_and_genre(df):
+    return df.groupby("Genre")[["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"]].sum()
+
+df = pd.read_csv('data/vgsales.csv')
+print(df.info())
 print(df.isnull().sum())
+
+df["Publisher"] = fill_na_publisher(df)
+print(df.isnull().sum())
+
+print(top_sales_global(df))
+
+print(sales_by_genres(df))
+
+print(sales_by_year(df))
+
+# It explain the lack of global sales since 2016, the dataset is not uploaded after this period
+# We have to ignore the results since 2016
+print(df.groupby("Year")["Name"].count())
+
+print(sales_by_region_and_genre(df))
